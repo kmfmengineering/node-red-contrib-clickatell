69ae736420f8cd87a2d1e07f51f11d948ab09ef8
Marc Bakos <marc.bakos@gmail.com>
2022-05-20 16:16:05 +0100

    Remove dependancy in the js

--- clickatell/clickatell.js
+++ clickatell/clickatell.js
@@ -5,7 +5,6 @@ module.exports = function(RED) {
 
     var DEBUG   = true;
     var MAXLEN  = 160;
-    var request = require('request');
 
     function ClickatellNode(n) {
         // Send SMS to Clickatell
